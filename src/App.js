6a43a00a0e4cc9f1f3532296da8c3a8f119d91dd
Vovkaa555 <vovkaa555@gmail.com>
2022-08-27 17:33:58 +0300

    Final version

--- src/App.js
+++ src/App.js
@@ -2,65 +2,18 @@ import React from 'react';
 import Home from './pages/Home';
 import styles from './App.module.scss';
 
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
-import { gapi } from 'gapi-script';
-
 export const SearchContext = React.createContext();
 
 function App() {
   const [searchValue, setSearchValue] = React.useState('');
-  //if login is true - show main page
-  const [profile, setProfile] = React.useState([]);
-  const [profileName, SetProfileName] = React.useState();
-  const clientId = '355600754553-rk677euu1a2pvums191rm3lbefa7q22u.apps.googleusercontent.com';
-  React.useEffect(() => {
-    const initClient = () => {
-      gapi.client.init({
-        clientId: clientId,
-        scope: '',
-      });
-    };
-    gapi.load('client:auth2', initClient);
-  });
-
-  const onSuccess = (res) => {
-    setProfile(res.profileObj);
-  };
-
-  const onFailure = (err) => {
-    console.log('failed', err);
-  };
-
-  const logOut = () => {
-    setProfile('');
-  };
 
   return (
     <div className={styles.root}>
       <SearchContext.Provider value={{ searchValue, setSearchValue }}>
         <div className={styles.app}>
-          {Boolean(profile) && profile.name !== undefined ? (
-            <div className={styles.main}>
-              <GoogleLogout
-                clientId={clientId}
-                className={styles.log_out}
-                buttonText="Log out"
-                onLogoutSuccess={logOut}
-              />
-              <Home searchValue={searchValue} {...profile} />
-            </div>
-          ) : (
-            <div className={styles.main}>
-              <GoogleLogin
-                clientId={clientId}
-                buttonText="Sign in with Google"
-                onSuccess={onSuccess}
-                onFailure={onFailure}
-                cookiePolicy={'single_host_origin'}
-                isSignedIn={true}
-              />
-            </div>
-          )}
+          <div className={styles.main}>
+            <Home searchValue={searchValue} />
+          </div>
         </div>
       </SearchContext.Provider>
     </div>
